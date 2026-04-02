fad1990016cd2650fdb6a727646bf9917a83268f
Axel Mrak <axelmrak@gmail.com>
2026-04-02 12:36:48 -0300

    add main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
+import Model.MultipleChoiceQuestion;
+import Model.TrueFalseQuestion;
+import Service.QuizService;
+
 class Main {
 
   public static void main(String[] args) {
+    QuizService quizService = new QuizService();
+
+    quizService.addQuestion(new MultipleChoiceQuestion(
+        "¿Cuál es la capital de Argentina?",
+        "Buenos Aires",
+        new String[] { "Buenos Aires", "Córdoba", "Rosario", "Mendoza" }));
+
+    quizService.addQuestion(new TrueFalseQuestion(
+        "Argentina es el país más grande de América del Sur.",
+        "Falso"));
+
+    quizService.addQuestion(new TrueFalseQuestion(
+        "El tango es un género musical originario de Argentina.",
+        "Verdadero"));
+
+    quizService.runQuiz();
 
   }
 }
