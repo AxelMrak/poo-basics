6d7fd87a929b5471a4eff6cdcfdc9d485c1b6c8c
Axel Mrak <axelmrak@gmail.com>
2026-04-02 16:24:13 -0300

    add scanner and update imports

--- src/service/QuizService.java
+++ src/service/QuizService.java
@@ -1,18 +1,22 @@
-package Service;
+package service;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
-import Model.MultipleChoiceQuestion;
-import Model.Question;
+import model.MultipleChoiceQuestion;
+import model.Question;
+import model.Util;
 
 public class QuizService {
 
-  private ArrayList<Question> questionsList;
+  private final ArrayList<Question> questionsList;
   private int score;
+  private final Scanner scanner;
 
   public QuizService() {
     this.questionsList = new ArrayList<>();
     this.score = 0;
+    this.scanner = new Scanner(System.in);
   }
 
   public void addQuestion(Question question) {
@@ -28,19 +32,44 @@ public class QuizService {
   }
 
   public void runQuiz() {
-    for (Question question : questionsList) {
+
+    for (int i = 0; i < questionsList.size(); i++) {
+      Question question = questionsList.get(i);
+
+      System.out.println("-----------------------------");
+      System.out.println("Pregunta " + (i + 1) + " de " + questionsList.size());
+      System.out.println("Puntuación actual: " + score);
+      System.out.println("-----------------------------");
       System.out.println(question.getQuestionText());
+
       if (question instanceof MultipleChoiceQuestion) {
         ((MultipleChoiceQuestion) question).showOptions();
       }
-      String userAnswer = System.console().readLine("Tu respuesta: ");
-      if (question.checkAnswer(userAnswer)) {
-        score++;
-        System.out.println("¡Respuesta correcta!");
-      } else {
-        System.out.println("Respuesta incorrecta. La respuesta correcta es: " + question.getCorrectAnswer());
+
+      boolean answered = false;
+      while (!answered) {
+        System.out.print("Tu respuesta: ");
+        String userAnswer = scanner.nextLine();
+
+        try {
+          if (question.checkAnswer(userAnswer)) {
+            score++;
+            System.out.println("¡Respuesta correcta!");
+          } else {
+            System.out.println("Respuesta incorrecta!");
+          }
+          answered = true;
+        } catch (IllegalArgumentException e) {
+          System.out.println(e.getMessage());
+          System.out.println("Intenta nuevamente.");
+        }
       }
+
+      Util.sleepSeconds(2);
+      Util.clearConsole();
     }
+
     System.out.println("Tu puntuación final es: " + score + "/" + questionsList.size());
+    Util.sleepSeconds(5);
   }
 }
